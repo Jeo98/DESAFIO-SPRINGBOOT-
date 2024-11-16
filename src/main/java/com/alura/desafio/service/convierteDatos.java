ff2b87c2adc4f8a57818835d6ca32078eb561f14
Jeo98 <evaristoolivares.jeo@gmail.com>
2024-11-16 15:41:03 -0300

    Estructura de conexión API creacion de interfaz y conversores de datos Json a genericos

---
+++ src/main/java/com/alura/desafio/service/convierteDatos.java
@@ -0,0 +1,30 @@
+package com.alura.desafio.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class convierteDatos implements  IFConvierteDatos{
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+
+    // permite mapear los valores que vienen de la API
+
+   /* public DatosSerie obtenerDatos(String json){
+       Cual es el problema aqui... no trabaja de forma generalizada, sino que esta funcion solo
+       trabajaria con DatosSerie. Supongamos que luego debo trabajar con DatosActores, tendré que crear
+       un metodo DatosActores para poder ver esos datos y asi con más información. Por ello es mejor
+       implementar una interface que trabaje con tipos de datos genéricos -->IconvierteDatos
+    }*/
+
+    @Override
+    public <T> T obtenerDatos(String json, Class<T> clase) {
+        try {
+            return objectMapper.readValue(json, clase);
+            //objectMapper va a leer el valor que viene en el json y lo convierte al tipo generico
+            //debe estar entre un try-catch por si ocurre error(intellij notifica del error si no está)
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
